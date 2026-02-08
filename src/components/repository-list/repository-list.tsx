24c095fd29d7405b3ef59b36205639dd80720989
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2026-02-08 23:45:09 +0900

    fix: add Body

--- src/components/repository-list/repository-list.tsx
+++ src/components/repository-list/repository-list.tsx
@@ -15,33 +15,35 @@ export const RepositoryList = ({ repositories }: RepositoryListProps) => {
         </Table.Row>
       </Table.Header>
 
-      {repositories.map((repo) => (
-        <Table.Row key={repo.id}>
-          <Table.Cell>
-            <Link
-              href={repo.html_url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {repo.full_name}
-            </Link>
-          </Table.Cell>
-          <Table.Cell>
-            {repo.created_at
-              ? new Date(repo.created_at).toLocaleString("ja-JP", {
-                  timeZone: "Asia/Tokyo",
-                  year: "numeric",
-                  month: "2-digit",
-                  day: "2-digit",
-                  hour: "2-digit",
-                  minute: "2-digit",
-                  second: "2-digit",
-                  hour12: false,
-                })
-              : "-"}
-          </Table.Cell>
-        </Table.Row>
-      ))}
+      <Table.Body>
+        {repositories.map((repo) => (
+          <Table.Row key={repo.id}>
+            <Table.Cell>
+              <Link
+                href={repo.html_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {repo.full_name}
+              </Link>
+            </Table.Cell>
+            <Table.Cell>
+              {repo.created_at
+                ? new Date(repo.created_at).toLocaleString("ja-JP", {
+                    timeZone: "Asia/Tokyo",
+                    year: "numeric",
+                    month: "2-digit",
+                    day: "2-digit",
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit",
+                    hour12: false,
+                  })
+                : "-"}
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
     </Table.Root>
   );
 };
