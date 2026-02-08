4675b385dd50bc232462e54d09cb4b964d8c4fa6
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2026-02-08 23:16:29 +0900

    feat: add Radix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import { Theme } from "@radix-ui/themes";
 
 export default function RootLayout({
   children,
@@ -7,7 +8,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Theme>{children}</Theme>
+      </body>
     </html>
   );
 }
